91db06b6d0c8d1d7d4eb85aaf14644d5feeaf848
bitwreckage <bitwreckage@users.noreply.github.com>
2025-05-07 21:58:49 +0000

    deploy: GjerdrumTKD/TkdQuiz@91038d3f16d5157dac7711e22031e342c69ba611

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1999071979"
+    serviceWorkerVersion: "3553122610"
   }
 });
